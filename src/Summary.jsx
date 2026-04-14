fc6a9701860f8d8960d5773b79a221149d48ea0a
nikitha chavvakula <chavvakulanikitha@gmail.com>
2026-04-14 18:04:15 +0530

    6th

--- src/Summary.jsx
+++ src/Summary.jsx
@@ -3,7 +3,7 @@ export default function Summary() {
     <div className="card">
       <h2>Objective</h2>
       <p>
-        Enthusiastic Computer Science student specializing in Artificial Intelligence and Machine Learning. 
+        Enthusiastic Computer Science professional specializing in Artificial Intelligence and Machine Learning. 
         Skilled in React.js, JavaScript, HTML, and CSS. Interested in AI/ML and chatbot development. 
         Seeking an entry-level opportunity to apply my skills and contribute to organizational growth.
       </p>
